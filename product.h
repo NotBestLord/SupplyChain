8d1130e6af31bfcccd0acf900b50c83270b0a73e
NotLord <amir01zx@gmail.com>
2026-06-02 14:58:43 +0300

    prodcut ctors, more transaction manager functions

--- product.h
+++ product.h
@@ -18,6 +18,12 @@ private:
 	int currentIngredients;
     Supplier* supplier;
 public:
+    Product(const char *name, double cost, 
+        double weight, int minAge, const Supplier *supplier);
+    Product(const Product& other);
+    Product(Product&& other);
+    ~Product();
+    
 	Product& operator=(const Product& other);
     friend istream& operator>>(istream& is, const Product& p);
     friend ostream& operator<<(ostream& os, const Product& p);
--- transaction_manager.h
+++ transaction_manager.h
@@ -12,13 +12,18 @@ class TransactionManager
 {
 private:
     Consumer** consumers;
-    int max_consumers;
+    int max_consumers; // if reached, double consumers array size and copy original values
     int current_consumers;
     Product** products;
-    int max_products;
+    int max_products; // if reached, double products array size and copy original values
     int current_products;
 public:
     void transact(Consumer& consumer, Product& product);
+    
+    void printConsumers();
+    void printProducts();
+    Consumer* getConsumer(int index);
+    Product* getProduct(const char *name);
 
     friend ostream& operator<<(ostream& os, const TransactionManager& set);
     const TransactionManager& operator+=(Consumer& consumer);
